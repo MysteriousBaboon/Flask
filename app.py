bfc82d9773496b9452e03b058daaa7670a4a20d2
MysteriousBaboon <46499985+MysteriousBaboon@users.noreply.github.com>
2024-01-19 04:57:59 +0100

    Self hosted Repo

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template
 import psycopg2
 import os
+import boto3
+from PIL import Image
+import io
 
 app = Flask(__name__)
-
-
-def get_db_connection():
-    conn = psycopg2.connect(host='',
-                            database='',
-                            user='',
-                            password='')
-    return conn
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+def get_db_connection():
+    conn = psycopg2.connect(host='mark.cfas4wq4cb4x.eu-west-3.rds.amazonaws.com',
+                            database='postgres',
+                            user='postgres',
+                            password='CGKKM4JejXYQ5PIRgC8a')
+    return conn
 
 
 @app.route("/student_list")
@@ -27,3 +30,10 @@ def student_list():
     cur.close()
     conn.close()
     return render_template('index.html', students=students)
+
+
+@app.route('/pinguin_and_poney')
+def pinguin_and_poney():
+    s3 = boto3.client('s3')
+    s3.download_file('bucketanuno', 'drapeau-anne-stokes-pirate-pr.png', 'nuno2.png')
+    return "<p>Chouette le poney!</p>"
